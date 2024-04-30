b5410cae4fb1ae1a45b569e42b08af67f3d50bfb
yhoungdev <yhoungdev@gmail.com>
2024-04-30 21:15:27 +0100

    chore: rounded up numbers

--- src/pages/views/previewToken.tsx
+++ src/pages/views/previewToken.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 //@ts-nocheck
-import { useState , useEffect} from "react";
+import { useState, useEffect } from "react";
 import { Header } from "../../components/layout/header";
 import Card from "../../components/misc/card";
 import FallBackMessage from "../../components/misc/fallbacks/isError";
@@ -20,14 +20,11 @@ import RiskAnalytics from "./riskAnalytics";
 import GetTokenOverview from "./tokenOverview";
 import { CHECKEDICON, DUSTICON } from "../../constants";
 
-
 const PreviewTokenPage = () => {
   const [isVoting, setIsVoting] = useState<false>(false);
   const searchParams: string = window.location.search.split("=")[1];
   const { connected } = useWallet();
   const walletAddress = useWallet().publicKey?.toString();
-
-
 
   const fetchAllRequest = async () => {
     const requests = [
@@ -91,138 +88,146 @@ const PreviewTokenPage = () => {
 
   useEffect(() => {
     document.title = `${tokenOverviewResponse?.Name || "Token Overview"}`;
-  })
+  });
   return (
     <>
       <Header />
       <div className="w-full h-full ">
-      <div className="container py-[1em]">
-            <div className="flex items-center justify-between w-full">
-              <h1 className="gradient-text text-2xl md:text-4xl w-full md:w-[30%]">
-                {isLoading && <IsSkeletonLoader count={1} />}
-                {isError && "Failed to Load Name"}
-                {data && tokenOverviewResponse?.Name}
-              </h1>
-            </div>
+        <div className="container py-[1em]">
+          <div className="flex items-center justify-between w-full">
+            <h1 className="gradient-text text-2xl md:text-4xl w-full md:w-[30%]">
+              {isLoading && <IsSkeletonLoader count={1} />}
+              {isError && "Failed to Load Name"}
+              {data && tokenOverviewResponse?.Name}
+            </h1>
+          </div>
 
-            <div className="mt-4">
-              <div className="flex flex-col md:flex-row items-center gap-5 md:gap-[2em] w-full">
-                <Card title="📦 Token Overview" className="w-full">
-                  <GetTokenOverview address={searchParams} />
-                </Card>
+          <div className="mt-4">
+            <div className="flex flex-col md:flex-row items-center gap-5 md:gap-[2em] w-full">
+              <Card title="📦 Token Overview" className="w-full">
+                <GetTokenOverview address={searchParams} />
+              </Card>
 
-                <Card title="📊 Community Sentiment " className="w-full">
-                  {!connected ? (
-                    <IsNotConnected />
-                  ) : (
-                    <div>
-                      {isError ? (
-                        <IsSkeletonLoader />
-                      ) : isError ? (
-                        <FallBackMessage />
-                      ) : (
-                        <div>
-                          <div className="text-center">
-                            <h1
-                              className="my-2 text-4xl bg-gray-800 p-3 w-fit 
+              <Card title="📊 Community Sentiment " className="w-full">
+                {!connected ? (
+                  <IsNotConnected />
+                ) : (
+                  <div>
+                    {isError ? (
+                      <IsSkeletonLoader />
+                    ) : isError ? (
+                      <FallBackMessage />
+                    ) : (
+                      <div>
+                        <div className="text-center">
+                          <h1
+                            className="my-2 text-4xl bg-gray-800 p-3 w-fit 
                                rounded-xl mx-auto"
-                            >
-                              {sentimentCheck ? DUSTICON : CHECKEDICON}
-                            </h1>
-                            <h4
-                              className={`${sentimentCheck ? "text-red-500 " : "text-green-500"} font-bold text-sm mb-3 `}
-                            >
-                              {sentimentCheck ? "TRASH" : "GOOD"}
-                            </h4>
-                          </div>
-                          {!isVoting ? (
-                            <div className="flex imtes-center gap-3 flex-col text-center">
-                              <div
-                                className="bg-text-white bg-red-900 px-4 
-                       cursor-pointer rounded py-5 font-bold font-semibol "
-                                onClick={() => handlePromiseOnVote()}
-                              >
-                                Red Flag 🚩
-                              </div>
-                              <div
-                                className="bg-text-white cursor-pointer bg-green-900 px-4 font-bold rounded py-5 font-semibol "
-                                onClick={() => castTokenVote(true)}
-                              >
-                                Bullish 🚀
-                              </div>
-                            </div>
-                          ) : (
-                            <div className="text-center">
-                              <h1 className="text-3xl">✋</h1>
-                              <h1 className="align-center mt-1 font-bold text-center">
-                                Voing in Progress
-                              </h1>
-                            </div>
-                          )}
+                          >
+                            {sentimentCheck ? DUSTICON : CHECKEDICON}
+                          </h1>
+                          <h4
+                            className={`${sentimentCheck ? "text-red-500 " : "text-green-500"} font-bold text-sm mb-3 `}
+                          >
+                            {sentimentCheck ? "TRASH" : "GOOD"}
+                          </h4>
                         </div>
-                      )}
-                    </div>
-                  )}
-                </Card>
-              </div>
-            </div>
-
-            <div className="flex  gap-4 flex-col md:flex-row mt-5 justify-between">
-              <div className=" w-full md:w-[55%]">
-                <Card title="🪙 Risk Analytics ">
-                  <RiskAnalytics
-                    tokenSentiment={sentimentResponse?.sentimentScore}
-                    tokenHoldersResponse={tokenHoldersResponse}
-                    tokenAddress={searchParams}
-                  />
-                </Card>
-              </div>
-
-              <div className=" w-full md:w-[50%]">
-                <Card title="🦈 Token Holders">
-                  {isLoading && <IsSkeletonLoader />}
-                  {isError && <FallBackMessage />}
-                  {tokenHoldersResponse && (
-                    <div className="flex items-center flex-col justify-between w-full overflow-x-auto h-[300px]">
-                      <div className="flex items-center justify-between w-full mb-4">
-                        <h1 className="font-bold">Address</h1>
-                        <h1 className="font-bold">Percentage</h1>
-                        <h1 className="font-bold">Amount</h1>
-                      </div>
-
-                      {/* @ts-ignore */}
-                      {tokenHoldersResponse?.map(
-                        (value: ITokenHolders, index: number) => {
-                          return (
+                        {!isVoting ? (
+                          <div className="flex imtes-center gap-3 flex-col text-center">
                             <div
-                              className="flex text-sm items-center justify-between w-full mt-3"
-                              key={index}
+                              className="bg-text-white bg-red-900 px-4 
+                       cursor-pointer rounded py-5 font-bold font-semibol "
+                              onClick={() => handlePromiseOnVote()}
                             >
-                              <a
-                                href={`https://solana.fm/address/${value?.address}`}
-                                target="_blank"
-                              >
-                                <h1 className="font-semibold text-yellow-500 bg-gray-800 p-1 ">
-                                  {value?.address.slice(0, 4)}...
-                                  {value?.address.slice(-5)}
-                                </h1>
-                              </a>
-                              <h1 className=" text-left">
-                                {Number(value?.percentage).toFixed(3)} %
-                              </h1>{" "}
-                              <h1 className="font-bold">
-                                 {numeral(value?.uiAmount).format("0, 0a")}
-                              </h1>{" "}
+                              Red Flag 🚩
                             </div>
-                          );
-                        },
-                      )}
-                    </div>
-                  )}
-                </Card>
-              </div>
+                            <div
+                              className="bg-text-white cursor-pointer bg-green-900 px-4 font-bold rounded py-5 font-semibol "
+                              onClick={() => castTokenVote(true)}
+                            >
+                              Bullish 🚀
+                            </div>
+                          </div>
+                        ) : (
+                          <div className="text-center">
+                            <h1 className="text-3xl">✋</h1>
+                            <h1 className="align-center mt-1 font-bold text-center">
+                              Voing in Progress
+                            </h1>
+                          </div>
+                        )}
+                      </div>
+                    )}
+                  </div>
+                )}
+              </Card>
             </div>
           </div>
+
+          <div className="flex  gap-4 flex-col md:flex-row mt-5 justify-between">
+            <div className=" w-full md:w-[55%]">
+              <Card title="🪙 Risk Analytics ">
+                <RiskAnalytics
+                  tokenSentiment={sentimentResponse?.sentimentScore}
+                  tokenHoldersResponse={tokenHoldersResponse}
+                  tokenAddress={searchParams}
+                />
+              </Card>
+            </div>
+
+            <div className=" w-full md:w-[50%]">
+              <Card title="🦈 Token Holders">
+                {isLoading && <IsSkeletonLoader />}
+                {isError && <FallBackMessage />}
+                {tokenHoldersResponse && (
+                  <div className="flex flex-col items-center justify-between w-full overflow-x-auto h-[300px]">
+                    <div className="flex items-center justify-between w-full mb-4">
+                      <h1 className="font-bold flex-1 text-left pl-4">
+                        Address
+                      </h1>
+                      <h1 className="font-bold flex-1 text-left">Percentage</h1>
+                      <h1 className="font-bold flex-1 text-left">Amount</h1>
+                    </div>
+
+                    {/* @ts-ignore */}
+                    {tokenHoldersResponse?.map(
+                      (value: ITokenHolders, index: number) => {
+                        return (
+                          <div
+                            className="flex text-sm items-center w-full mt-3 text-left"
+                            key={index}
+                          >
+                            <a
+                              href={`https://solana.fm/address/${value?.address}`}
+                              target="_blank"
+                              className="flex-1"
+                            >
+                              <h1 className="font-semibold text-yellow-500 bg-gray-800 p-1 w-fit px-3">
+                                {value?.address.slice(0, 4)}...
+                                {value?.address.slice(-5)}
+                              </h1>
+                            </a>
+                            <h1 className="text-left flex-1">
+                              {Number(value?.percentage).toFixed(2)} %
+                            </h1>
+                            <h1 className="font-bold flex-1">
+                              {numeral(value?.uiAmount).format("0,0a")}
+                            </h1>
+                          </div>
+                        );
+                      },
+                    )}
+                  </div>
+                )}
+              </Card>
+            </div>
+            
+          </div>
+          
+
+          <div></div>
+        </div>
+        
       </div>
     </>
   );
